956624e8b4caf5e5e8d3200d286890b79dd80d9c
pandeyjee2310 <pandeyjee2310@gmail.com>
2024-08-11 15:48:02 +0000

    updated vite.config file for production build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,15 +3,18 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		port: 3000,
-		proxy: {
-			"/api": {
-				target: "http://localhost:9002", // Backend server URL
-				changeOrigin: true,
-				secure: false,
-			},
-		},
-	},
+  plugins: [react()],
+  build: {
+    outDir: 'dist',  // Ensure the output directory is specified for Vercel
+  },
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:9002", // Backend server URL for development
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
