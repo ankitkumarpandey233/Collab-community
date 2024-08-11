c3d967cdc0ba9d56872b74c68fd478744e68007d
pandeyjee2310 <pandeyjee2310@gmail.com>
2024-08-11 20:06:11 +0000

    changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',  // Output directory for the build
-    emptyOutDir: true,  // Ensure the directory is cleaned before building
-  },
   server: {
     port: 3000,
     proxy: {
